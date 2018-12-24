fb544b026a537c3f3d75a7d9c08e871c545c0b04
Peyton Walters <pwpon500@gmail.com>
2018-12-23 23:28:47 -0500

    Made scoring smart (not brute force), added extra die rolls

--- main.go
+++ main.go
@@ -5,137 +5,103 @@ import "fmt"
 var (
 	dice     = 3
 	curScore = 350
+	maxDepth = 2
 )
 
 func main() {
-	fmt.Println(backtrack([]int{}, dice))
+	//fmt.Println(backtrack([]int{}, dice, curScore))
+	fmt.Println(scoreRoll([]int{1, 2, 3, 4, 5, 6}, 0, 0))
 }
 
 // backtrack to find all possible roll combinations for a given number of dice
-func backtrack(rolls []int, toRoll int) float64 {
+func backtrack(rolls []int, toRoll int, score int, depth int) float64 {
 	if toRoll == 0 {
-		return float64(maxScore(rolls))
+		return float64(scoreRoll(rolls, score, depth))
+	}
+	if depth >= maxDepth {
+		return 0
 	}
 
 	sum := 0.0
 	for i := 1; i <= 6; i++ {
-		sum += backtrack(append(rolls, i), toRoll-1)
+		sum += backtrack(append(rolls, i), toRoll-1, score, depth)
 	}
 	return sum / 6
 }
 
-// find the maximum score for a given roll set
-func maxScore(rolls []int) int {
-	max := 0
-	partitions := generatePartitions(rolls)
-	for _, partition := range partitions {
-		score := scoreSuperSet(partition)
-		if score > max {
-			max = score
+// score a roll by finding the number of occurences of each number and scoring roll
+func scoreRoll(rolls []int, score int, depth int) float64 {
+	var occurences [6]int
+	for _, elem := range rolls {
+		occurences[elem-1]++
+	}
+	var frequencies [7][]int
+
+	for i := 0; i < 6; i++ {
+		occurs := occurences[i]
+		frequencies[occurs] = append(frequencies[occurs], i)
+	}
+
+	toReturn := 0.0
+	countIndiv := true
+	numUsed := 0
+	if len(frequencies[6]) > 0 {
+		toReturn += 3000
+		numUsed += 6
+	} else if len(frequencies[5]) > 0 {
+		toReturn += 2000
+		numUsed += 5
+	} else if len(frequencies[4]) > 0 && len(frequencies[2]) > 0 {
+		toReturn += 1500
+		countIndiv = false
+		numUsed += 6
+	} else if len(frequencies[4]) > 0 {
+		toReturn += 1000
+		numUsed += 4
+	} else if len(frequencies[3]) == 2 {
+		toReturn += 2500
+		numUsed += 6
+	} else if len(frequencies[2]) == 3 {
+		toReturn += 1500
+		countIndiv = false
+		numUsed += 6
+	} else if len(frequencies[3]) == 1 {
+		if frequencies[3][0] == 0 {
+			toReturn += 300
+			numUsed += 3
+		} else {
+			toReturn += float64(100 * (frequencies[3][0] + 1))
+			numUsed += 3
 		}
+	} else if len(frequencies[1]) == 6 {
+		toReturn += 1500
+		countIndiv = false
+		numUsed += 6
 	}
 
-	if max > 0 {
-		return curScore + max
+	if occurences[0] < 3 && countIndiv {
+		toReturn += float64(100 * occurences[0])
+		numUsed += occurences[0]
 	}
-	return 0
-}
-
-// find all the ways to partition a given roll in order to score each partition
-func generatePartitions(rolls []int) [][][]int {
-	if len(rolls) == 1 {
-		return [][][]int{{{rolls[0]}}}
+	if occurences[4] < 3 && countIndiv {
+		toReturn += float64(50 * occurences[4])
+		numUsed += occurences[4]
 	}
 
-	firstElem := rolls[0]
-	rest := generatePartitions(rolls[1:])
-	toReturn := [][][]int{}
+	if toReturn == 0 {
+		return 0
+	}
 
-	for _, elem := range rest {
-		toReturn = append(toReturn, append(elem, []int{firstElem}))
+	numLeft := len(rolls) - numUsed
+	if numLeft == 0 {
+		numLeft = 6
+	}
 
-		for i, set := range elem {
-			removed := make([][]int, len(elem))
-			copy(removed, elem[:])
-			removed = append(removed[:i], removed[i+1:]...)
-			toReturn = append(toReturn, append(removed, append(set, firstElem)))
-		}
+	toReturn += float64(score)
+	expectedRoll := backtrack([]int{}, numLeft, int(toReturn), depth+1)
+	if expectedRoll > float64(toReturn) {
+		toReturn = expectedRoll
 	}
 
 	return toReturn
-}
-
-// score all the partitions
-func scoreSuperSet(rolls [][]int) int {
-	if len(rolls) == 2 {
-		if len(rolls[0]) == 3 && len(rolls[1]) == 3 {
-			if rolls[0][0] == rolls[0][1] && rolls[0][1] == rolls[0][2] && rolls[1][0] == rolls[1][1] && rolls[1][1] == rolls[1][2] {
-				return curScore + 2500
-			}
-		}
-		if len(rolls[0]) == 4 && len(rolls[1]) == 2 {
-			if rolls[0][0] == rolls[0][1] && rolls[0][1] == rolls[0][2] && rolls[0][2] == rolls[0][3] && rolls[1][0] == rolls[1][1] {
-				return curScore + 1500
-			}
-		}
-	}
-
-	if len(rolls) == 3 {
-		if len(rolls[0]) == 2 && len(rolls[1]) == 2 && len(rolls[2]) == 2 {
-			if rolls[0][0] == rolls[0][1] && rolls[1][0] == rolls[1][1] && rolls[2][0] == rolls[2][1] {
-				return curScore + 1500
-			}
-		}
-	}
-
-	sum := 0
-	for _, elem := range rolls {
-		sum += scoreSet(elem)
-	}
-
-	return sum
-}
-
-// score a single partition
-func scoreSet(rolls []int) int {
-	if allEqual(rolls) {
-		if len(rolls) == 6 {
-			return 3000
-		}
-		if len(rolls) == 5 {
-			return 2000
-		}
-		if len(rolls) == 4 {
-			return 1000
-		}
-		if len(rolls) == 3 {
-			if rolls[0] == 1 {
-				return 300
-			}
-
-			return rolls[0] * 100
-		}
-	}
-
-	sum := 0
-	for _, roll := range rolls {
-		if roll == 5 {
-			sum += 50
-		}
-		if roll == 1 {
-			sum += 100
-		}
-	}
-
-	return sum
-}
-
-// check if all rolls in a partition are the same
-func allEqual(rolls []int) bool {
-	for i := 0; i < len(rolls)-1; i++ {
-		if rolls[i] != rolls[i+1] {
-			return false
-		}
-	}
-	return true
 }
